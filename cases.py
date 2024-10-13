42fd68ee73360f47e13f7a687457a1df603abd5d
Dmitriy Sulzhyts <dsulzhits@gmail.com>
2024-10-13 13:28:53 +0300

    Добавлен cases.py

---
+++ cases.py
@@ -0,0 +1,42 @@
+# str_data = "привет я строка!"
+# str_data_upper = str_data.upper()
+# print(str_data_upper)
+#
+# str_data = "ПРИВЕТ Я СТРОКА!"
+# str_data_lower = str_data.lower()
+# print(str_data_lower)
+#
+# str_data_1 = "привет я строка!"
+# str_data_2 = "ПРИВЕТ Я СТРОКА!"
+# str_data_cap_1 = str_data_1.capitalize()
+# str_data_cap_2 = str_data_2.capitalize()
+# print(str_data_cap_1)
+# print(str_data_cap_2)
+#
+#
+# str_data_1 = "привет я строка!"
+# str_data_2 = "ПРИВЕТ Я СТРОКА!"
+# str_data_title_1 = str_data_1.title()
+# str_data_title_2 = str_data_2.title()
+# print(str_data_title_1)
+# print(str_data_title_2)
+
+
+# str_data = "   ПРИВЕТ Я СТРОКА!    "
+# print(str_data)
+# str_data_strip = str_data.strip()
+# print(str_data_strip)
+# str_data_lstrip = str_data.lstrip()
+# print(str_data_lstrip)
+# str_data_rstrip = str_data.rstrip()
+# print(str_data_rstrip)
+
+
+str_data = "   ПРИВЕТ Я СТРОКА!    "
+print(str_data)
+str_data_strip_l = str_data.lower().strip()
+print(str_data_strip_l)
+str_data_lstrip = str_data.lstrip().capitalize()
+print(str_data_lstrip)
+str_data_rstrip = str_data.rstrip().title()
+print(str_data_rstrip)
